4878387eebcb7c8e0a207635fa5d0a2855b90926
renn72 <simplesam8856@gmail.com>
2024-09-11 18:23:23 +1000

    lift schema

---
+++ src/server/db/schema/lift.ts
@@ -0,0 +1,49 @@
+import { sql, relations } from 'drizzle-orm'
+import { index, int, sqliteTableCreator, text } from 'drizzle-orm/sqlite-core'
+import { user } from './user'
+import { entry } from './entry'
+import { competition } from './competition'
+
+export const createTable = sqliteTableCreator((name) => `good-lyft_${name}`)
+
+export const lift = createTable(
+  'lift',
+  {
+    id: int('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+    createdAt: int('created_at', { mode: 'timestamp' })
+      .default(sql`(unixepoch())`)
+      .notNull(),
+    competitionId: int('competition_id', { mode: 'number' }).references(
+      () => competition.id,
+      {
+        onDelete: 'cascade',
+      },
+    ),
+    entryId: int('entry_id', { mode: 'number' }).references(() => entry.id, {
+      onDelete: 'cascade',
+    }),
+    userId: int('user_id', { mode: 'number' }).references(() => user.id, {
+      onDelete: 'cascade',
+    }),
+    liftName: text('lift_name'),
+    liftNumber: int('lift_number'),
+    weight: text('weight'),
+    isOne: int('is_one', { mode: 'boolean' }),
+    isTwo: int('is_two', { mode: 'boolean' }),
+    isThree: int('is_three', { mode: 'boolean' }),
+    order: int('order'),
+    bracket: int('bracket'),
+    day: int('day'),
+    platform: int('platform'),
+    rackHeight: text('rack_height'),
+    gender: text('gender'),
+    entryWeight: int('entry_weight'),
+    notes: text('notes'),
+  },
+  (e) => {
+    return {
+      competitionidIdx: index('lift_competitionid_idx').on(e.competitionId),
+      entryidIdx: index('lift_entryid_idx').on(e.entryId),
+    }
+  },
+)
